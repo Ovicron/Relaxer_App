212ea0093de2490f67dd50d537d987a82723afbc
Ovicron <Ovicrondw@gmail.com>
2020-06-08 12:28:56 -0400

    all

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
 
-let container = document.querySelector('.container')
-let text = document.querySelector('#text')
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
 
-let totalTime = 7500
-let breatheTime = (totalTime / 5) * 2
-let holdTime = totalTime / 5
+breathAnimation();
 
-breatheAnimation()
-
-function breatheAnimation() {
-    text.innerHTML = 'Breathe In!'
-    container.className = 'container grow'
+function breathAnimation() {
+    text.innerText = 'Breathe In!';
+    container.className = 'container grow';
 
     setTimeout(() => {
-        text.innerHTML = 'Hold!'
+        text.innerText = 'Hold';
 
         setTimeout(() => {
-            text.innerHTML = 'Breathe Out!'
-            container.className = 'container shrink'
-        }, holdTime)
-    }, breatheTime)
+            text.innerText = 'Breathe Out!';
+            container.className = 'container shrink';
+        }, holdTime);
+    }, breatheTime);
 }
 
-setInterval(breatheAnimation, totalTime)
+setInterval(breathAnimation, totalTime);
